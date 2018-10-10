bfcf5e3c22485a04f9f5e6dab6093ffb9bbb7f80
Monty Anderson <work@montyanderson.net>
2018-10-10 15:57:40 +0100

    Merged album template into app

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,159 @@
 <template>
 	<div>
-		<pre v-for="log in logs">{{log}}</pre>
+			  <section class="jumbotron text-center">
+				<div class="container">
+				  <h1 class="jumbotron-heading">Album example</h1>
+				  <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+				  <p>
+					<a href="#" class="btn btn-primary my-2">Main call to action</a>
+					<a href="#" class="btn btn-secondary my-2">Secondary action</a>
+				  </p>
+				</div>
+			  </section>
+
+			  <div class="album py-5 bg-light">
+				<div class="container">
+				  <h4>Sources</h4>
+				  <div class="row">
+					<div class="col-md-4">
+					  <div class="card mb-4 shadow-sm">
+						<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+						<div class="card-body">
+
+							<div class="input-group card-text" style="padding-bottom: 15px;">
+							  <div class="input-group-prepend">
+								<div class="input-group-text">URL:</div>
+							  </div>
+							  <input type="text" class="form-control col-xs-12" id="inlineFormInputGroup" placeholder="URL for a JPEG stream.">
+							</div>
+
+							<div class="input-group card-text" style="padding-bottom: 15px;">
+							  <div class="input-group-prepend">
+								<div class="input-group-text">FPS:</div>
+							  </div>
+							  <input type="text" class="form-control col-xs-12" id="inlineFormInputGroup" value="1">
+							</div>
+
+						  <div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+							  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+							  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+							</div>
+							<small class="text-muted">9 mins</small>
+						  </div>
+						</div>
+					  </div>
+					</div>
+					<div class="col-md-4">
+					  <div class="card mb-4 shadow-sm">
+						<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+						<div class="card-body">
+						  <div  style="padding-bottom: 15px;">
+							  <div class="custom-file  card-text">
+		  <input type="file" class="custom-file-input" id="customFile" multiple>
+		  <label class="custom-file-label" for="customFile">Choose file</label>
+		</div>
+		</div>
+
+						  <div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+							  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+							  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+							</div>
+							<small class="text-muted">9 mins</small>
+						  </div>
+						</div>
+					  </div>
+					</div>
+					<div class="col-md-4">
+					  <div class="card mb-4 shadow-sm">
+						<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+						<div class="card-body">
+						  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						  <div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+							  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+							  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+							</div>
+							<small class="text-muted">9 mins</small>
+						  </div>
+						</div>
+					  </div>
+					</div>
+
+				  </div>
+
+				  <h4>Transform</h4>
+				  <div class="row">
+					<div class="col-md-4">
+					  <div class="card mb-4 shadow-sm">
+						<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+						<div class="card-body">
+
+							<div class="input-group card-text" style="padding-bottom: 15px;">
+							  <div class="input-group-prepend">
+								<div class="input-group-text">URL:</div>
+							  </div>
+							  <input type="text" class="form-control col-xs-12" id="inlineFormInputGroup" placeholder="URL for a JPEG stream.">
+							</div>
+
+							<div class="input-group card-text" style="padding-bottom: 15px;">
+							  <div class="input-group-prepend">
+								<div class="input-group-text">FPS:</div>
+							  </div>
+							  <input type="text" class="form-control col-xs-12" id="inlineFormInputGroup" value="1">
+							</div>
+
+						  <div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+							  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+							  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+							</div>
+							<small class="text-muted">9 mins</small>
+						  </div>
+						</div>
+					  </div>
+					</div>
+					<div class="col-md-4">
+					  <div class="card mb-4 shadow-sm">
+						<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+						<div class="card-body">
+						  <div  style="padding-bottom: 15px;">
+							  <div class="custom-file  card-text">
+		  <input type="file" class="custom-file-input" id="customFile" multiple>
+		  <label class="custom-file-label" for="customFile">Choose file</label>
+		</div>
+		</div>
+
+						  <div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+							  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+							  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+							</div>
+							<small class="text-muted">9 mins</small>
+						  </div>
+						</div>
+					  </div>
+					</div>
+					<div class="col-md-4">
+					  <div class="card mb-4 shadow-sm">
+						<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+						<div class="card-body">
+						  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						  <div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+							  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+							  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+							</div>
+							<small class="text-muted">9 mins</small>
+						  </div>
+						</div>
+					  </div>
+					</div>
+
+				  </div>
+				</div>
+			  </div>
 	</div>
 </template>
 
